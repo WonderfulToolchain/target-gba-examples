b273b20afec80fedbdec0e6b1ad543f6d25f1fc3
Adrian Siekierka <kontakt@asie.pl>
2023-10-29 14:04:01 +0100

    templates/gba: update

---
+++ templates/gba-library/include/libexample.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Adrian "asie" Siekierka, 2023
+
+#ifndef __LIBEXAMPLE_H__
+#define __LIBEXAMPLE_H__
+
+int libexample_installed(void);
+
+#endif // __LIBEXAMPLE_H__
---
+++ templates/gba-library/src/library.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: CC0-1.0
+//
+// SPDX-FileContributor: Adrian "asie" Siekierka, 2023
+
+int libexample_installed(void) {
+	return 1;
+}
